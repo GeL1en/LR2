d9c5ff4021e6e54a88f1c9be5f1671afb479b03c
GeL1en <makslogvinovich@gmail.com>
2024-10-15 13:57:42 +0300

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Ввод координат точки
+Console.WriteLine("Введите x=");
+float x = float.Parse(Console.ReadLine());
+Console.WriteLine("Введите y=");
+float y = float.Parse(Console.ReadLine());
+// Проверка на границу
+if ((y == x + 2 && x >= -2 && x <= 0) || (y == -x + 2 && x >= 0 && x <= 2) || (y == 0 && x >= -2 && x <= 2))
+{
+    Console.WriteLine("Точка на границе");
+}
+// Проверка внутри области
+else if (x >= -2 && x <= 2 && y >= 0 && y <= 2 && y <= x + 2 && y <= -x + 2)
+{
+    Console.WriteLine("Точка входит в область");
+}
+// Если ни одно условие не выполнено, значит, точка вне области
+else
+{
+    Console.WriteLine("Точка не входит в область");
+}
